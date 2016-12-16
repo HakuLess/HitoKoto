02d7629ebbbcc08da7e494b50204cc9d44a0c834
马双飞 <mashuangfei@meituan.com>
2016-12-16 17:24:40 +0800

    增加其他方法，改变状态，改变座位绑定

--- routes/where.js
+++ routes/where.js
@@ -9,6 +9,69 @@ router.get('/', function(req, res, next) {
     getSeats(res);
 });
 
+router.get('/bind', function(req, res, next) {
+    bind(req, res);
+});
+
+router.get('/change', function(req, res, next) {
+    change(req, res);
+});
+
+// 改变用户在线状态
+function change(req, res) {
+
+	var mis = req.query.mis;
+	var seatStatus = req.query.seatStatus;
+
+	var updateStr = 'update where_db set seatStatus = ' 
+		+ seatStatus + ' where user_mis = "' + mis + '"';
+
+	console.log(updateStr);
+
+	pool.query(updateStr, 
+
+		function(err, rows, fields) {
+	  		if (err) throw err;
+			res.json({
+			    "result":"success"
+			})
+		});
+	}
+
+// 修改相应数据内容
+function bind(req, res) {
+	var row = req.query.row;
+	var col = req.query.col;
+	var name = req.query.name;
+	var mis = req.query.mis;
+	var avatar = req.query.avatar;
+	var seatStatus = req.query.seatStatus;
+
+	var updateStr = 'update where_db set user_mis = "' 
+		+ mis + '"'
+		+ ', user_name = "' + name + '"';
+	if (avatar != null && avatar != "") {
+		updateStr += ', user_avatar = "' + avatar + '"';
+	}
+	if (seatStatus != null && seatStatus != "") {
+		updateStr += ', seatStatus = "' + seatStatus + '"';
+	}
+	updateStr = updateStr
+		+ ' where row = ' + row 
+		+ ' and col = ' + col;
+
+	console.log(updateStr);
+
+	pool.query(updateStr, 
+
+		function(err, rows, fields) {
+	  		if (err) throw err;
+			res.json({
+			    "result":"success"
+			})
+		});
+	}
+
 function Seat() {
 	this.seatStatus = 0;
 	this.user = new User();
@@ -38,14 +101,18 @@ function getSeats(res) {
 					seats = new Array();
 				}
 				
-				console.log(rows[i].row + " " + rows[i].col + rows[i].user_name);
+				// console.log(rows[i].row + " " + rows[i].col + rows[i].user_name);
 
 				var seat = new Seat();
 				seat.seatStatus = rows[i].seatStatus;
-				seat.user.name = rows[i].user_name;
-				seat.user.mis = rows[i].user_mis;
-				seat.user.avatar = rows[i].user_avatar;
 
+				// 只有在状态为1的座位才有 员工信息
+				if (seat.seatStatus >= 1) {
+					seat.user.name = rows[i].user_name;
+					seat.user.mis = rows[i].user_mis;
+					seat.user.avatar = rows[i].user_avatar;
+				}
+				
 				seats.push(seat);
 			}
 			seatsArray.push(seats);
